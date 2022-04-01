1585902d59719f8c4e739393b0941e56893dbba0
Pagelloo <102830683+Pagelloo@users.noreply.github.com>
2022-04-01 22:04:34 +0200

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,12 @@
+echo "# TestNet" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Pagelloo/TestNet.git
+git push -u origin main
+
+
+git remote add origin https://github.com/Pagelloo/TestNet.git
+git branch -M main
+git push -u origin main
