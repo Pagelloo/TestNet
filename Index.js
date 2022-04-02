376a3bdcdba658abe4ab5e3f374da2041a6e21bd
Pagelloo <102830683+Pagelloo@users.noreply.github.com>
2022-04-02 08:11:47 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,12 +1,9 @@
-echo "# TestNet" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Pagelloo/TestNet.git
-git push -u origin main
-
-
-git remote add origin https://github.com/Pagelloo/TestNet.git
-git branch -M main
-git push -u origin main
+        $(document).ready(function(){
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
+});
